6c64b51ab074428c776131721a87f5c3851a7fd3
Ankita Sogani <jainankita148@gmail.com>
2022-03-12 17:44:55 +0530

    initial commit

--- MVCFluentValidationDemo/Models/EmployeeValidator.cs
+++ MVCFluentValidationDemo/Models/EmployeeValidator.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace MVCFluentValidationDemo.Models
 {
-    public class EmployeeValidator :AbstractValidator<Employee>
+    public class EmployeeValidator : AbstractValidator<Employee>
     {
         public EmployeeValidator()
         {
@@ -14,18 +14,19 @@ namespace MVCFluentValidationDemo.Models
             RuleFor(x => x.LastName).NotEmpty().WithMessage("Last Name is Required");
             RuleFor(x => x.CompanyName).NotEmpty().WithMessage("Company Name is Required");
             RuleFor(x => x.DOB).NotEmpty().WithMessage("Date of Birth is Required");
-            RuleFor(x => x.DOB).Must(ValidateAge).WithMessage("Employee age must be 18 or greater than 18");
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email is Required");
-            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not valid");            
             RuleFor(x => x.Salary).NotEmpty().WithMessage("Salary is Required");
-            RuleFor(x => x.Salary).GreaterThan(0).WithMessage("Employee Salary should be greater than 0");
-        }
 
+            RuleFor(x => x.CompanyName).Length(5, 500).WithMessage("Company Name must be between 0 to 500 character");
+            RuleFor(x => x.DOB).Must(ValidateAge).WithMessage("Employee age must be 18 or greater than 18");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not valid");
+            RuleFor(x => x.Salary).GreaterThan(1000).WithMessage("Employee Salary should be greater than 1000");
+        }
         private bool ValidateAge(DateTime age)
         {
             DateTime dateTime = DateTime.Today;
             int actualAge = dateTime.Year - Convert.ToDateTime(age).Year;
-            if(actualAge < 18)
+            if (actualAge < 18)
             {
                 return false;
             }
