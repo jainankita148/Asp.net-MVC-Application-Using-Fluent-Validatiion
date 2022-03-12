5edfc59a00e9ab0b9f0f5fae6591aec186e519df
Ankita Sogani <ankitasogani.vision@gmail.com>
2022-03-12 15:50:07 +0530

    initial commit

---
+++ MVCFluentValidationDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+using MVCFluentValidationDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFluentValidationDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee employee)
+        {
+            EmployeeValidator validationRules = new EmployeeValidator();
+            ValidationResult result = validationRules.Validate(employee);
+            if (!result.IsValid)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View(employee);
+        }
+
+    }
+}
---
+++ MVCFluentValidationDemo/Models/EmployeeValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFluentValidationDemo.Models
+{
+    public class EmployeeValidator :AbstractValidator<Employee>
+    {
+        public EmployeeValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First Name is Required");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Last Name is Required");
+            RuleFor(x => x.CompanyName).NotEmpty().WithMessage("Company Name is Required");
+            RuleFor(x => x.DOB).NotEmpty().WithMessage("Date of Birth is Required");
+            RuleFor(x => x.DOB).Must(ValidateAge).WithMessage("Employee age must be 18 or greater than 18");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is Required");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not valid");            
+            RuleFor(x => x.Salary).NotEmpty().WithMessage("Salary is Required");
+            RuleFor(x => x.Salary).GreaterThan(0).WithMessage("Employee Salary should be greater than 0");
+        }
+
+        private bool ValidateAge(DateTime age)
+        {
+            DateTime dateTime = DateTime.Today;
+            int actualAge = dateTime.Year - Convert.ToDateTime(age).Year;
+            if(actualAge < 18)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
